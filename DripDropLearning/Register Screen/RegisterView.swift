dd1b4c4b45ed044a290f507d6c04b440515c3456
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2023-06-19 17:42:10 -0700

    Creating Register View

---
+++ DripDropLearning/Register Screen/RegisterView.swift
@@ -0,0 +1,87 @@
+//
+//  RegisterView.swift
+//  DripDropLearning
+//
+//  Created by Kayla Nguyen on 6/19/23.
+//
+
+import UIKit
+
+class RegisterView: UIView {
+    var textfieldRegisterName: UITextField!
+    var textfieldRegisterEmail: UITextField!
+    var textfieldRegisterPass: UITextField!
+    var buttonCreateAccount: UIButton!
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        setupTextFieldRegisterName()
+        setupTextFieldRegisterEmail()
+        setupTextFieldRegisterPass()
+        setupButtonCreateAccount()
+        
+        initConstraints()
+    }
+    
+    func setupTextFieldRegisterName(){
+        textfieldRegisterName = UITextField()
+        textfieldRegisterName.placeholder = "Name"
+        textfieldRegisterName.borderStyle = .roundedRect
+        textfieldRegisterName.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(textfieldRegisterName)
+    }
+    
+    func setupTextFieldRegisterEmail(){
+        textfieldRegisterEmail = UITextField()
+        textfieldRegisterEmail.placeholder = "Email"
+        textfieldRegisterEmail.borderStyle = .roundedRect
+        textfieldRegisterEmail.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(textfieldRegisterEmail)
+    }
+    
+    func setupTextFieldRegisterPass(){
+        textfieldRegisterPass = UITextField()
+        textfieldRegisterPass.placeholder = "Password"
+        textfieldRegisterPass.borderStyle = .roundedRect
+        textfieldRegisterPass.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(textfieldRegisterPass)
+    }
+    
+    func setupButtonCreateAccount(){
+        buttonCreateAccount = UIButton(type: .system)
+        buttonCreateAccount.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        buttonCreateAccount.setTitle("Create Account", for: .normal)
+        buttonCreateAccount.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(buttonCreateAccount)
+    }
+    
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+            
+            textfieldRegisterName.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 32),
+            textfieldRegisterName.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            textfieldRegisterName.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            textfieldRegisterEmail.topAnchor.constraint(equalTo: textfieldRegisterName.bottomAnchor, constant: 16),
+            textfieldRegisterEmail.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            textfieldRegisterEmail.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            textfieldRegisterPass.topAnchor.constraint(equalTo: textfieldRegisterEmail.bottomAnchor, constant: 16),
+            textfieldRegisterPass.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            textfieldRegisterPass.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+
+            buttonCreateAccount.topAnchor.constraint(equalTo: textfieldRegisterPass.bottomAnchor, constant: 16),
+            buttonCreateAccount.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 100),
+            buttonCreateAccount.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -100),
+           
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
---
+++ DripDropLearning/Register Screen/RegisterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RegisterViewController.swift
+//  DripDropLearning
+//
+//  Created by Kayla Nguyen on 6/19/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func loadView() {
+        view = RegisterView()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
